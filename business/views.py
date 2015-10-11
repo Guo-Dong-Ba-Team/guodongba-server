b1a6cf26dc1c5a60c9770f67e240fd63c67f7aae
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2015-10-11 16:59:06 +0800

    move login and register model to business app

--- business/views.py
+++ business/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def register(request):
+	return render_to_response('registration/register.html')
+
+def login(request):
+	return render_to_response('registration/login.html')
+
